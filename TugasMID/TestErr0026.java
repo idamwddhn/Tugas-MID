e93058ec7f83a0fcd6e6c08dec06e63b4a19368c
idamwddhn <127532576+idamwddhn@users.noreply.github.com>
2023-03-29 14:02:38 +0800

    TestErr0026.java

---
+++ TugasMID/TestErr0026.java
@@ -0,0 +1,17 @@
+/* Nama : Ida Mawaddah Nur
+   NIM  : 13020210026 */
+
+import java.util.Scanner;
+
+public class TestErr0026 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        int k = 100;
+        char c = 's';
+        boolean falses = true;
+        int j = i + 1;
+        System.out.println("j adalah " + j + " k adalah " + k + " dan c adalah " + c + " dan trues adalah " + falses);
+    }
+}
+
